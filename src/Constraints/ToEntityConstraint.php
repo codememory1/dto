6cb415e0c048a6168637aaa0904f19e4a0a14f00
Danil <kostynd1@gmail.com>
2023-02-25 16:46:02 +0300

    Констреинт ToEntity разделен на две части ToEntityList и ToEntity

--- src/Constraints/ToEntityConstraint.php
+++ src/Constraints/ToEntityConstraint.php
@@ -9,13 +9,10 @@ use Codememory\Dto\Interfaces\ConstraintInterface;
 final class ToEntityConstraint implements ConstraintInterface
 {
     public function __construct(
-        public readonly string $byKey,
-        public readonly bool $isList = false,
-        public readonly bool $uniqueInList = true,
-        public readonly bool $checkNotFoundEntity = true,
-        public readonly ?string $customHandlerNotFoundEntity = null, /* Method name of DataTransfer */
-        public readonly ?string $itemValueConverter = null,          /* Method name of DataTransfer */
-        public readonly ?string $entity = null
+        public readonly ?string $entity = null,
+        public readonly ?string $byKey = null,
+        public readonly ?string $whereCallback = null,         /* Method name of DataTransfer */
+        public readonly ?string $entityNotFoundCallback = null /* Method name of DataTransfer */
     ) {
     }
 
